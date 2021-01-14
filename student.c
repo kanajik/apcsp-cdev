6f1b83d0784bd2662a71ec9613361333795b58d6
Kai Kanaji <kanajik4148@my.sduhsd.net>
2021-01-14 04:55:32 +0000

    student.c

---
+++ student.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+
+
+// student structure
+//struct Student...
+struct Student {
+char firstName[50];
+char lastName[50];
+int age[100];
+};
+
+void printStudent(struct Student* student)
+{
+printf("Student: %s %s\n", student->firstName, student->lastName);
+printf("Age: %d\n", student->age);
+}
+
+
+void printAllStudents(struct Student students[], int num)
+{
+  // call printStudent for each student in students
+for (int i = 0; i < num; i++) {
+printStudent(students);
+}
+}
+
+
+int main()
+{
+char input[256];
+char fname[50];
+char lname[50];
+char age[100];
+  // an array of students
+  //xxx students;
+
+  int numStudents = 0;
+  while (1)
+  {
+    char c;
+    printf("\nEnter a to add, p to print, q to quit:");
+    fgets(input, 256, stdin);
+    if (sscanf(input, "%c", &c) != 1) continue;
+    if (c == 'q') 
+    {
+      break;
+    }
+    else if (c == 'p')
+    {
+      // print all students
+      printAllStudents(students, numStudents);
+      continue;
+    }
+    else if (c == 'a')
+    {
+      // enter a new student
+printf("First name: ");
+fgets(input, 50, stdin);
+sscanf(input, "%s", fname);
+
+printf("Last name: ");
+fgets(input, 50, stdin);
+sscanf(input, "%s", lname);
+
+printf("Age: ");
+fgets(input, 100, stdin);
+sscanf(input, "%d", age);
+
+      numStudents++;
+    ;}
+  }
+  
+  printf("\nGoodbye!\n");
+}
